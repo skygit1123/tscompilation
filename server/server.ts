6b6ff2097ef5594e8a8493ff12ea04c3961b03b6
sky <skymsdn@hotmail.com>
2020-10-30 09:31:36 +0800

    added @types, compile and serve script

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,21 @@
 import * as express from "express";
+import {Question} from "../@types/Question";
 
+
+const questions: Question[] = [
+{
+    title: "How to login",
+    content: "How do i login",
+    answerCount: 2
+}
+];
 const port : string | number = process.env.port || 1337;
 
 const app = express();
 app.use(express.static("public"));
 app.listen(port);
 console.log("Listening on port " + port);
+
+app.get("/questions", (_req, res)=> {
+    res.json(questions);
+});  
