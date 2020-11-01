758a9ca9992d02c8b90af673b0f28020b90e0980
sky <skymsdn@hotmail.com>
2020-11-01 11:07:41 +0800

    clear linting errors

--- server/server.ts
+++ server/server.ts
@@ -3,11 +3,11 @@ import * as path from "path";
 import { Question } from "../@types/Question";
 
 const questions: Question[] = [
-{
+  {
     title: "How to login",
     content: "How do i login",
     answerCount: 2
-}
+  }
 ];
 const port : string | number = process.env.port || 1337;
 
@@ -16,11 +16,10 @@ app.use(express.static("public"));
 app.listen(port);
 console.log("Listening on port " + port);
 
-app.get("/questions", (_req, res)=> {
-    debugger;
-    res.json(questions);
-});  
+app.get("/questions", (_req, res) => {
+  res.json(questions);
+});
 
 app.get("/main.js", (_req, res) => {
-    res.sendFile(path.resolve(__dirname,"..", "client", "client.js"));
+  res.sendFile(path.resolve(__dirname, "..", "client", "client.js"));
 });
